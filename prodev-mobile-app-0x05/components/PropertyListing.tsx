a17ca2f48aa79d9d2a80b6873f55fb4b4591dd02
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-09-07 15:34:50 +0100

    Modified navigation using tabs navigation patterns

--- prodev-mobile-app-0x05/components/PropertyListing.tsx
+++ prodev-mobile-app-0x05/components/PropertyListing.tsx
@@ -1,10 +1,16 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import PropertyListingCard from "./common/PropertyListingCard";
-import { PropertyListing } from "../interfaces";
+import { useState } from "react";
+import { PropertListing, PropertyListingProps } from "../interfaces";
 
-const PropertyListingProp = ({ listings }: PropertyListing) => {
+const PropertyListing = ({ listings }: PropertListing) => {
   return (
-    <View style={{ flex: 1, rowGap: 10 }}>
+    <View
+      style={{
+        flex: 1,
+        rowGap: 10,
+      }}
+    >
       {listings?.map((listing, key) => (
         <PropertyListingCard {...listing} key={key} />
       ))}
@@ -12,4 +18,4 @@ const PropertyListingProp = ({ listings }: PropertyListing) => {
   );
 };
 
-export default PropertyListingProp;
+export default PropertyListing;
