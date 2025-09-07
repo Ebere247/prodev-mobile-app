176c82df3781ebf00544be7b02c5d098ff427296
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-09-07 21:21:55 +0100

    Styling with nativewind (tailwindcss)

---
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -0,0 +1,7 @@
+import BottomNavigation from "@/components/layout/Navigations";
+import "@/styles/global.css";
+import React from "react";
+
+export default function HomeLayout() {
+  return <BottomNavigation />;
+}
